b2bb48e402213244abb199164970ccb866a5ec01
pkl0912 <pklpkl0912@gmail.com>
2024-07-25 20:30:38 +0900

    first commit

---
+++ src/test/java/com/example/spring_login_study/SpringLoginStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_login_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
